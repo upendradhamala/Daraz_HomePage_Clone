d3dfe13b419fb4b1ac99ac23a7494efc11468212
Upendra <dhamalaupendra@gmail.com>
2021-06-01 16:26:39 +0545

    Made Navbar

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TopNav from '../components/Navbar/TopNav'
+const Home = () => {
+  return (
+    <div className='home-containery'>
+      <TopNav />
+    </div>
+  )
+}
+
+export default Home
